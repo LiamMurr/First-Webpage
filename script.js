1e0d150d0eda4f294b7d353cf5c9df721faea70b
Liam Murray <murray.liam5426@gmail.com>
2024-10-13 11:26:01 +1000

    added script.js file, styled contact form.

---
+++ script.js
@@ -0,0 +1,10 @@
+// const name = document.getElementById('name')
+// const email = document.getElementById('email')
+// const tel = document.getElementById('tel')
+// const form = document.getElementById('form')
+// const service = document.getElementById('service')
+// const textArea = document.getElementById('messagetext')
+
+
+//to top of page
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
